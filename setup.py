1ed2ac30ff2ea02a05519cc6f1745e5d31aa90d1
Ryan Gartin <ryan@eagledevelopers.net>
2020-07-19 23:36:40 -0400

    change setup

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import pathlib
 from setuptools import setup
 
-setup(name='folding_at_home',
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='folding_at_home_client_api',
       version='1.1.0',
       description='API and console app which wraps Folding@Home Client API for Pause/Restart/Power commands',
+      long_description=README,
+      long_description_content_type="text/markdown",
       keywords='folding folding@home foldingathome',
       url='https://github.com/nitrag/folding-at-home-api-python',
       author='Ryan Gartin',
