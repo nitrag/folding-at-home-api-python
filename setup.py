2a9e73bcd1bb4ec7b3c0f7b4ce5fd2183e1d07b0
Ryan Gartin <ryan@eagledevelopers.net>
2020-07-19 23:49:31 -0400

    Setup Tweaks

--- setup.py
+++ setup.py
@@ -5,25 +5,26 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / 'README.md').read_text()
 
-setup(name='folding_at_home_client_api',
-      version='1.1.0',
-      description='API and console app which wraps Folding@Home Client API for Pause/Restart/Power commands',
-      long_description=README,
-      long_description_content_type="text/markdown",
-      keywords='folding folding@home foldingathome',
-      url='https://github.com/nitrag/folding-at-home-api-python',
-      author='Ryan Gartin',
-      author_email='ryan@eagledevelopers.net',
-      license='MIT',
-      install_requires=[
-          'argparse',
-          'requests'
-      ],
-      entry_points={
-          'console_scripts': [
-              'folding-at-home = folding_at_home.main:main'
-          ]
-      },
-      zip_safe=False)
+setup(
+    name='folding_at_home',
+    version='1.2.0',
+    description='API and console tool which wraps Folding@Home Client API for Pause/Restart/Power commands',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    keywords='folding folding@home foldingathome',
+    url='https://github.com/nitrag/folding-at-home-api-python',
+    author='Ryan Gartin',
+    author_email='ryan@eagledevelopers.net',
+    license='MIT',
+    install_requires=[
+      'argparse',
+      'requests'
+    ],
+    entry_points={
+      'console_scripts': [
+          'folding-at-home = folding_at_home.main:main'
+      ]
+    },
+    python_requires='>=3.6')
